dbbaff8b45729d93fb7e8b7858090c7a371e5c34
chinmaykumarsahoo77 <chinmayasahoo9777@gmail.com>
2023-11-19 00:29:33 +0530

    Created view and associated with urls.py using router of rest_framework.

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MovieSerializer
+from .models import Moviedata
 
 # Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    #It's going to have a 'queryset' and a 'serializer_class'
+    queryset = Moviedata.objects.all()
+    serializer_class = MovieSerializer
+
